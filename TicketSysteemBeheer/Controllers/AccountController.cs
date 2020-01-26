360537898947ade598223b1d448bc916e6a6aa49
Paul B <p_botusan@live.nl>
2020-01-26 11:26:13 +0100

    afronding beheer

--- TicketSysteemBeheer/Controllers/AccountController.cs
+++ TicketSysteemBeheer/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace TicketSysteemBeheer.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Voornaam = model.Voornaam, Achternaam = model.Achternaam };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
